921ba409e5c945efc1410480126dfaa29cc12359
Reece Robinson <reecekrobinson@gmail.com>
2025-09-19 11:56:52 -0800

    overhawl

---
+++ home-manager/home.nix
@@ -0,0 +1,116 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "reece";
+  home.homeDirectory = "/var/home/reece";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    pkgs.neovim
+    pkgs.bat
+    pkgs.ghostty
+    # pkgs.gnomeExtensions.pop-shell 
+    # pkgs.swaynotificationcenter
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/reece/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Hyprland
+  # wayland.windowManager.hyprland.enable = true; # enable Hyprland
+  # programs.waybar.enable = true;
+  # programs.kitty.enable = true; # required for the default Hyprland config
+  # wayland.windowManager.hyprland.settings = {
+
+  # Vi Mode
+  # programs.broot.settings.modal = true;
+  programs.zsh.setOptions = [ "VI" ];
+
+  # ZSH
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    history.size = 10000;
+
+    shellAliases = {
+      ll = "ls -l";
+      ls = "ls --color=auto";
+      doy = "date +%j";
+      gpsweek = "echo \"$((($(date +%s) - 315964800) / 604800 ))\"";
+    };
+
+    zplug = {
+      enable = true;
+      plugins = [
+        # Autocompletion
+        { name = "zsh-users/zsh-autosuggestions"; }
+        # Pure
+        { name = "mafredri/zsh-async"; }
+        { name = "sindresorhus/pure"; tags=[use:pure.zsh as:theme]; }
+      ];
+    };
+  };
+}
