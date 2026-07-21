b537e3ee140742019b2b17aa6908fd196da85dbe
Reece Robinson <reecekrobinson@gmail.com>
2026-07-20 17:44:54 -0800

    add nix files

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,1 +1,240 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  unstable = import <unstable> { config = config.nixpkgs.config; };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixpad";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
+
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+        ControllerMode = "bredr";
+        Experimental = true;
+      };
+    };
+  };
+
+  # NVIDIA
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true; # try false if issues
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Anchorage";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Wifi printing
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.meslo-lg
+    nerd-fonts.sauce-code-pro
+  ];
+
+  programs = {
+    virt-manager.enable = true;
+    firefox.enable = true;
+    niri.enable = true;
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        stdenv.cc.cc.lib
+      ];
+    };
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions = {
+      	enable = true;
+      };
+    };
+  };
+
+
+  # Run waybar under systemd so it auto-restarts after its crash on monitor
+  # sleep/unplug (output removal segfault in waybar::Client::main). Replaces
+  # niri's spawn-at-startup, which does not respawn it.
+  systemd.user.services.waybar = {
+    description = "Waybar";
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.waybar}/bin/waybar";
+      ExecReload = "kill -SIGUSR2 $MAINPID";
+      Restart = "on-failure";
+      RestartSec = 1;
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = (with pkgs; [
+    alacritty
+    arduino-cli
+    atuin
+    atuin-desktop
+    awscli2
+    bat
+    bind
+    blueman
+    cameractrls
+    cliphist
+    curl
+    dbeaver-bin
+    devbox
+    devpod
+    discord
+    fuzzel
+    gcc
+    git
+    git-lfs
+    google-chrome
+    htop
+    inotify-tools
+    jq
+    logseq
+    neovim
+    openconnect
+    openvpn
+    orca-slicer
+    # overskride
+    pavucontrol
+    pkg-config
+    playerctl
+    podman-compose
+    pure-prompt
+    python3
+    ripgrep
+    sox
+    spotify
+    stdenv.cc.cc.lib
+    stow
+    sunsetr
+    swaynotificationcenter
+    swww
+    unzip
+    usbutils
+    waybar
+    waypaper
+    wget
+    wl-clipboard
+    xwayland-satellite
+    zellij
+  ]) ++ [
+    unstable.voxtype
+  ];
+
+  environment.sessionVariables = {
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.reece = {
+    isNormalUser = true;
+    description = "Reece";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd"];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Enable common container config files in /etc/containers
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    libvirtd = {
+      enable = true;
+    };
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  system.stateVersion = "25.11";
+}
